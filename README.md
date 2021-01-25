765942d255c56c64ecd7ac1651e6970df5a3f404
jpitkanen18 <jpitkanen18@gmail.com>
2021-01-25 15:57:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Epic Push to talk bypass tool v1.0.0 HACKER420
+
+Epic tool to bypass discord push to talk (if bound to right arrow);
+
+## Requirements:
+1. A Computer
+3. Node.js
+4. Time(?)
+7. Patience if it doesn't work
+
+## Installation and running
+
+<pre><code>#:~ git clone https://github.com/jpitkanen18/EpicPTT.git
+#:~ cd EpicPTT
+#:~ npm install
+#:~ npm run rebuild
+#:~ npm start
+</code></pre>
+
---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+
+const nodeAbi = require('node-abi')
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+const {ipcMain} = require('electron');
+const { keyboard, Key, mouse, left, right, up, down, screen } = require("@nut-tree/nut-js");
+// Attach listener in the main process with the given ID
+
+ipcMain.on('request-mainprocess-action', (event, arg) => {
+    if(arg.message === true){
+        keyboard.pressKey(Key.Right);
+    } else {
+      keyboard.releaseKey(Key.Right);
+    }
+    
+});
